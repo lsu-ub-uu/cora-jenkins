c992cef5b1d57c9fe3588c2dfa486df19eb49d77
perebartrolisimo <pere.bartroli@ub.uu.se>
2021-02-03 15:37:02 +0000

    indexloader is now parametrized

--- shell/indexloader.sh
+++ shell/indexloader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ../../cora-indexloader/target
 
-java -cp cora-indexloader-0.1-SNAPSHOT-jar-with-dependencies.jar se.uu.ub.cora.indexloader.index.IndexerBatchRunner "${AUTH_TOKEN}" "${APPTOKEN_URL}" "${DIVA_REST}" "se.uu.ub.cora.javaclient.cora.CoraClientFactoryImp" "se.uu.ub.cora.indexloader.index.DataIndexerImp" "${RECORD_TYPE}"
+echo "java -cp cora-indexloader-0.1-SNAPSHOT-jar-with-dependencies.jar se.uu.ub.cora.indexloader.index.IndexerBatchRunner "${AUTH_TOKEN}" "${APPTOKEN_URL}" "${DIVA_REST}" "se.uu.ub.cora.javaclient.cora.CoraClientFactoryImp" "se.uu.ub.cora.indexloader.index.DataIndexerImp" "${RECORD_TYPE}"" | bash
