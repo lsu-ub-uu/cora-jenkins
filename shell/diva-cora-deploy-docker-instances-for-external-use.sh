cce091234732690da5f97787cb40b63544d93c82
belanglos <benjamin@longstream.se>
2021-02-18 10:17:16 +0000

    Setting names on cora-docker-synchronizer.

--- shell/diva-cora-deploy-docker-instances-for-external-use.sh
+++ shell/diva-cora-deploy-docker-instances-for-external-use.sh
@@ -12,9 +12,9 @@ docker run --net=diva-cora --restart always -e "JAVA_OPTS=-Dapptokenverifier.pub
 #idplogin
 docker run --net=diva-cora --restart always -e "JAVA_OPTS=-Dmain.system.domain=https://cora.epc.ub.uu.se -Dtoken.logout.url=https://cora.epc.ub.uu.se/diva/apptokenverifier/rest/apptoken/" -p 8612:8009 --name diva-idplogin --link diva-gatekeeper:gatekeeper -d  cora-docker-idplogin:1.0-SNAPSHOT
 #synchronizer
-docker run --net=diva-cora --restart always --name diva-docker-synchronizer -e "JAVA_OPTS=-DapptokenVerifierURL=http://diva-apptokenverifier:8080/apptokenverifier/ -DbaseURL=http://diva-cora:8080/diva/rest/ -DbuserId=${USER_ID} -DappToken=${AUTH_TOKEN}" -d cora-docker-synchronizer:1.0-SNAPSHOT
+docker run --net=diva-cora --restart always --name diva-synchronizer -e "JAVA_OPTS=-DapptokenVerifierURL=http://diva-apptokenverifier:8080/apptokenverifier/ -DbaseURL=http://diva-cora:8080/diva/rest/ -DbuserId=${USER_ID} -DappToken=${AUTH_TOKEN}" -d cora-docker-synchronizer:1.0-SNAPSHOT
 #fitnesse
-docker run --net=diva-cora --restart always  --volumes-from diva-cora -p 8690:8090 --name diva-fitnesse --link diva-cora:diva --link diva-apptokenverifier:apptokenverifier --link diva-idplogin:idplogin -e tokenLogoutURL=https://cora.epc.ub.uu.se/diva/apptokenverifier/rest/apptoken/ -d diva-cora-docker-fitnesse:1.0-SNAPSHOT
+docker run --net=diva-cora --restart always  --volumes-from diva-cora -p 8690:8090 --name diva-fitnesse --link diva-cora:diva --link diva-apptokenverifier:apptokenverifier --link diva-idplogin:idplogin --link diva-synchronizer:synchronizer -e tokenLogoutURL=https://cora.epc.ub.uu.se/diva/apptokenverifier/rest/apptoken/ -d diva-cora-docker-fitnesse:1.0-SNAPSHOT
 #fedora with db
 docker run --net=diva-cora --restart always -e POSTGRES_DB=fedora32 -e POSTGRES_USER=fedoraAdmin -e POSTGRES_PASSWORD=fedora --name diva-cora-postgresql -d diva-cora-docker-fcrepo-postgresql:1.0.0
 #wait for fedora db to start
--- shell/diva-cora-deploy-docker-instances-for-internal-use.sh
+++ shell/diva-cora-deploy-docker-instances-for-internal-use.sh
@@ -12,9 +12,9 @@ docker run --net=diva-cora-test --volumes-from diva-therest-test --name diva-app
 #idplogin
 docker run --net=diva-cora-test --name diva-idplogin-test --link diva-gatekeeper-test:gatekeeper -e "JAVA_OPTS=-Dtoken.logout.url=https://apptokenverifier/rest/" -d  cora-docker-idplogin:1.0-SNAPSHOT
 #synchronizer
-docker run --net=diva-cora-test --name diva-docker-synchronizer -e "JAVA_OPTS=-DapptokenVerifierURL=http://diva-apptokenverifier-test:8080/apptokenverifier/ -DbaseURL=http://diva-therest-test:8080/diva/rest/ -DbuserId=${USER_ID} -DappToken=${AUTH_TOKEN}" -d cora-docker-synchronizer:1.0-SNAPSHOT
+docker run --net=diva-cora-test --name diva-synchronizer-test -e "JAVA_OPTS=-DapptokenVerifierURL=http://diva-apptokenverifier-test:8080/apptokenverifier/ -DbaseURL=http://diva-therest-test:8080/diva/rest/ -DbuserId=${USER_ID} -DappToken=${AUTH_TOKEN}" -d cora-docker-synchronizer:1.0-SNAPSHOT
 #fitnesse
-docker run --net=diva-cora-test -p 8590:8090 --name diva-fitnesse-test --link diva-therest-test:diva --link diva-apptokenverifier-test:apptokenverifier --link diva-idplogin-test:idplogin -e tokenLogoutURL=https://apptokenverifier/rest/ -d diva-cora-docker-fitnesse:1.0-SNAPSHOT
+docker run --net=diva-cora-test -p 8590:8090 --name diva-fitnesse-test --link diva-therest-test:diva --link diva-apptokenverifier-test:apptokenverifier --link diva-idplogin-test:idplogin --link diva-synchronizer-test:synchronizer -e tokenLogoutURL=https://apptokenverifier/rest/ -d diva-cora-docker-fitnesse:1.0-SNAPSHOT
 #fedora with db
 docker run --net=diva-cora-test --restart always -e POSTGRES_DB=fedora32 -e POSTGRES_USER=fedoraAdmin -e POSTGRES_PASSWORD=fedora --name diva-cora-postgresql-test -d diva-cora-docker-fcrepo-postgresql:1.0.0
 #wait for fedora db to start
