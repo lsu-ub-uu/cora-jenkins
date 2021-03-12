21aed025368f386b4d76bc541c4f0a98d91e9e65
perebartrolisimo <pere.bartroli@ub.uu.se>
2021-03-12 12:04:46 +0000

    dockerJavaVersion script

--- shell/dockerJavaVersion.sh
+++ shell/dockerJavaVersion.sh
@@ -1,4 +1,3 @@
-!/usr/bin/env bash
 set -e
 DOCKERS="alvin-fitnesse alvin-cora-fedora alvin-cora-postgresql alvin-cora-docker-postgresql alvin-solr alvin-apptokenverifier alvin-idplogin alvin-gatekeeper alvin-cora diva-cora-docker-postgresql diva-fitnesse diva-cora-fedora diva-cora-postgresql diva-solr diva-apptokenverifier diva-idplogin diva-gatekeeper diva-cora systemone-fitnesse solr apptokenverifier idplogin gatekeeper systemone diva-jsclient jsclient alvin-jsclient"
 
