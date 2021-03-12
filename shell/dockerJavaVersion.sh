2f591b86a1cfa327435ae7006b7982317e5ff8cc
perebartrolisimo <pere.bartroli@ub.uu.se>
2021-03-12 12:21:48 +0000

    dockerJavaVersion script

--- shell/dockerJavaVersion.sh
+++ shell/dockerJavaVersion.sh
@@ -1,6 +1,6 @@
 set -e
-DOCKERS="fitnesse therest systemone-fitnesse systemone solr idplogin apptokenverifier gatekeeper alvin-fitnesse alvin-cora alvin-cora-fedora alvin-solr alvin-idplogin alvin-apptokenverifier alvin-gatekeeper diva-fitnesse diva-cora diva-cora-fedora diva-solr diva-idplogin diva-apptokenverifier diva-gatekeeper diva-synchronizer"
-#excluded = alvin-cora-postgresql, alvin-cora-docker-postgresql, diva-cora-docker-postgresql, diva-cora-postgresql
+DOCKERS="therest systemone-fitnesse systemone solr idplogin apptokenverifier gatekeeper alvin-fitnesse alvin-cora alvin-cora-fedora alvin-solr alvin-idplogin alvin-apptokenverifier alvin-gatekeeper diva-fitnesse diva-cora diva-cora-fedora diva-solr diva-idplogin diva-apptokenverifier diva-gatekeeper diva-synchronizer"
+#excluded = alvin-cora-postgresql, alvin-cora-docker-postgresql, diva-cora-docker-postgresql, diva-cora-postgresql fitnesse
 
 
 for DOCKER in $DOCKERS
