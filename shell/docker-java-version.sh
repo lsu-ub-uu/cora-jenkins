cc33bf0bc6332fbcfe76b80a5c333561b708a40e
perebartrolisimo <pere.bartroli@ub.uu.se>
2021-03-12 11:52:24 +0000

    docker-java-version script

---
+++ shell/docker-java-version.sh
@@ -0,0 +1,47 @@
+
+docker exec alvin-fitnesse java -version
+echo ""
+docker exec alvin-cora-fedora java -version
+echo ""
+#docker exec alvin-cora-postgresql java -version
+#docker exec alvin-cora-docker-postgresql java -version
+#docker exec alvin-solr java -version
+docker exec alvin-apptokenverifier java -version
+echo ""
+docker exec alvin-idplogin java -version
+echo ""
+docker exec alvin-gatekeeper java -version
+echo ""
+docker exec alvin-cora java -version
+echo ""
+#docker exec diva-cora-docker-postgresql java -version
+docker exec diva-fitnesse java -version
+echo ""
+docker exec diva-cora-fedora java -version
+echo ""
+#docker exec diva-cora-postgresql java -version
+#docker exec diva-solr java -version
+docker exec diva-apptokenverifier java -version
+echo ""
+docker exec diva-idplogin java -version
+echo ""
+docker exec diva-gatekeeper java -version
+echo ""
+docker exec diva-cora java -version
+echo ""
+docker exec systemone-fitnesse java -version
+echo ""
+#docker exec solr java -version
+docker exec apptokenverifier java -version
+echo ""
+docker exec idplogin java -version
+echo ""
+docker exec gatekeeper java -version
+echo ""
+docker exec systemone java -version
+echo ""
+docker exec diva-jsclient java -version
+echo ""
+docker exec jsclient java -version
+echo ""
+docker exec alvin-jsclient java -version
