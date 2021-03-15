a663d9c35c9576d334a5de88de27e3cb8750db0d
olovm <olov.mckie@ub.uu.se>
2021-03-15 12:32:11 +0000

    changing order to match between internal and external

--- shell/alvin-cora-deploy-docker-instances-for-external-use.sh
+++ shell/alvin-cora-deploy-docker-instances-for-external-use.sh
@@ -1,6 +1,7 @@
 docker stop alvin-fitnesse alvin-cora alvin-cora-fedora alvin-cora-postgresql alvin-solr alvin-idplogin alvin-apptokenverifier alvin-gatekeeper alvin-cora-docker-postgresql && echo nothingToSeeMoveOnToNextCommand
 docker rm alvin-fitnesse alvin-cora alvin-cora-fedora alvin-cora-postgresql alvin-solr alvin-idplogin alvin-apptokenverifier alvin-gatekeeper alvin-cora-docker-postgresql && echo nothingToSeeMoveOnToNextCommand
 docker volume rm $(docker volume ls -q) && echo nothingToSeeMoveOnToNextCommand
+
 docker run --net=alvin-cora --restart always  -v /mnt/data/basicstorage -p 8410:8009 --name alvin-cora --link alvin-gatekeeper:gatekeeper --link alvin-solr:solr --link alvin-cora-fedora:alvin-cora-fedora --link alvin-cora-docker-postgresql:alvin-cora-docker-postgresql -d  alvin-docker-cora:1.0-SNAPSHOT
 docker run --net=alvin-cora --restart always --volumes-from alvin-cora --name alvin-gatekeeper --link alvin-cora-docker-postgresql:alvin-cora-docker-postgresql -d  alvin-docker-gatekeeper:1.0-SNAPSHOT
 docker run --net=alvin-cora --restart always -e "JAVA_OPTS=-Dmain.system.domain=https://cora.epc.ub.uu.se -Dtoken.logout.url=https://cora.epc.ub.uu.se/alvin/apptokenverifier/rest/apptoken/" -p 8412:8009 --name alvin-idplogin --link alvin-gatekeeper:gatekeeper -d  cora-docker-idplogin:1.0-SNAPSHOT
--- shell/alvin-cora-deploy-docker-instances-for-internal-use.sh
+++ shell/alvin-cora-deploy-docker-instances-for-internal-use.sh
@@ -3,16 +3,15 @@ docker rm alvin-fitnesse-test alvin-therest-test alvin-solr-test alvin-apptokenv
 docker volume rm $(docker volume ls -q) && echo nothingToSeeMoveOnToNextCommand
 
 docker run --net=alvin-cora-test -v /mnt/data/basicstorage --name alvin-therest-test --link alvin-gatekeeper-test:gatekeeper --link alvin-solr-test:solr --link alvin-cora-fedora-test:alvin-cora-fedora --link alvin-cora-docker-postgresql-test:alvin-cora-docker-postgresql -d  alvin-docker-cora:1.0-SNAPSHOT
-#docker run --net=alvin-cora-test --name alvin-solr-test -d solr solr-create -c coracore
-docker run --net=alvin-cora-test --name alvin-solr-test -d cora-solr:1.0-SNAPSHOT solr-precreate coracore /opt/solr/server/solr/configsets/coradefaultcore
 docker run --net=alvin-cora-test --volumes-from alvin-therest-test --name alvin-gatekeeper-test --link alvin-cora-docker-postgresql-test:alvin-cora-docker-postgresql -d  alvin-docker-gatekeeper:1.0-SNAPSHOT
-docker run --net=alvin-cora-test --volumes-from alvin-therest-test --name alvin-apptokenverifier-test --link alvin-gatekeeper-test:gatekeeper -d  cora-docker-apptokenverifier:1.0-SNAPSHOT
-docker run --net=alvin-cora-test --restart always -e POSTGRES_DB=alvin -e POSTGRES_USER=alvin -e POSTGRES_PASSWORD=alvin --name alvin-cora-docker-postgresql-test -d alvin-cora-docker-postgresql-9.6
-docker run --net=alvin-cora-test --restart always -e POSTGRES_DB=fedora38 -e POSTGRES_USER=fedoraAdmin -e POSTGRES_PASSWORD=fedora --name alvin-cora-postgresql-test -d cora-docker-postgresql:9.6
 docker run --net=alvin-cora-test --name alvin-idplogin-test --link alvin-gatekeeper-test:gatekeeper -e "JAVA_OPTS=-Dtoken.logout.url=https://apptokenverifier/rest/" -d  cora-docker-idplogin:1.0-SNAPSHOT
+docker run --net=alvin-cora-test --volumes-from alvin-therest-test --name alvin-apptokenverifier-test --link alvin-gatekeeper-test:gatekeeper -d  cora-docker-apptokenverifier:1.0-SNAPSHOT
+docker run --net=alvin-cora-test --name alvin-solr-test -d cora-solr:1.0-SNAPSHOT solr-precreate coracore /opt/solr/server/solr/configsets/coradefaultcore
+docker run --net=alvin-cora-test --restart always -e POSTGRES_DB=alvin -e POSTGRES_USER=alvin -e POSTGRES_PASSWORD=alvin --name alvin-cora-docker-postgresql-test -d alvin-cora-docker-postgresql-9.6:1.0-SNAPSHOT
+docker run --net=alvin-cora-test --restart always -e POSTGRES_DB=fedora38 -e POSTGRES_USER=fedoraAdmin -e POSTGRES_PASSWORD=fedora --name alvin-cora-postgresql-test -d cora-docker-postgresql:9.6
 #wait for fedora db to start
 sleep 25
-docker run --net=alvin-cora-test --restart always --name alvin-cora-fedora-test --link alvin-cora-postgresql-test:postgres-fcrepo -d alvin-cora-docker-fedora-3.8.1:2.2.1
+docker run --net=alvin-cora-test --restart always --name alvin-cora-fedora-test --link alvin-cora-postgresql-test:postgres-fcrepo -d alvin-cora-docker-fedora-3.8.1:2.3-SNAPSHOT
 #wait for fedora to start
 sleep 25
 docker run --net=alvin-cora-test -p 8390:8090 --name alvin-fitnesse-test --link alvin-therest-test:alvin --link alvin-apptokenverifier-test:apptokenverifier --link alvin-idplogin-test:idplogin -e tokenLogoutURL=https://apptokenverifier/rest/ -d alvin-cora-docker-fitnesse:1.1-SNAPSHOT
