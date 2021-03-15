bc50c5368822bcb164f894f517d59c0c3bae17ac
olovm <olov.mckie@ub.uu.se>
2021-03-15 10:53:24 +0000

    more time for startup to try to fix problems with place not found

--- shell/alvin-cora-deploy-docker-instances-for-internal-use.sh
+++ shell/alvin-cora-deploy-docker-instances-for-internal-use.sh
@@ -11,10 +11,10 @@ docker run --net=alvin-cora-test --restart always -e POSTGRES_DB=alvin -e POSTGR
 docker run --net=alvin-cora-test --restart always -e POSTGRES_DB=fedora38 -e POSTGRES_USER=fedoraAdmin -e POSTGRES_PASSWORD=fedora --name alvin-cora-postgresql-test -d cora-docker-postgresql:9.6
 docker run --net=alvin-cora-test --name alvin-idplogin-test --link alvin-gatekeeper-test:gatekeeper -e "JAVA_OPTS=-Dtoken.logout.url=https://apptokenverifier/rest/" -d  cora-docker-idplogin:1.0-SNAPSHOT
 #wait for fedora db to start
-sleep 15
+sleep 25
 docker run --net=alvin-cora-test --restart always --name alvin-cora-fedora-test --link alvin-cora-postgresql-test:postgres-fcrepo -d alvin-cora-docker-fedora-3.8.1:2.2.1
 #wait for fedora to start
-sleep 15
+sleep 25
 docker run --net=alvin-cora-test -p 8390:8090 --name alvin-fitnesse-test --link alvin-therest-test:alvin --link alvin-apptokenverifier-test:apptokenverifier --link alvin-idplogin-test:idplogin -e tokenLogoutURL=https://apptokenverifier/rest/ -d alvin-cora-docker-fitnesse:1.1-SNAPSHOT
 
 #wait for everything to start
