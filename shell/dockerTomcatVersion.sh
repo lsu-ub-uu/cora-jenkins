5d7c7de5f2737fbb79a50a3ce7673f0dfa0a1fe2
perebartrolisimo <pere.bartroli@ub.uu.se>
2021-08-24 14:25:03 +0000

    upgrade to tomcat 10

--- shell/dockerTomcatVersion.sh
+++ shell/dockerTomcatVersion.sh
@@ -1,6 +1,6 @@
 set -e
 #DOCKERS="systemone-fitnesse systemone solr idplogin apptokenverifier gatekeeper alvin-fitnesse alvin-cora alvin-cora-fedora alvin-solr alvin-idplogin alvin-apptokenverifier alvin-gatekeeper diva-fitnesse diva-cora diva-cora-fedora diva-solr diva-idplogin diva-apptokenverifier diva-gatekeeper diva-synchronizer jsclient diva-jsclient diva-jsclient"
-DOCKERS="systemone diva-jsclient"
+DOCKERS="systemone idplogin apptokenverifier gatekeeper alvin-cora alvin-idplogin alvin-apptokenverifier alvin-gatekeeper diva-cora diva-idplogin diva-apptokenverifier diva-gatekeeper diva-synchronizer jsclient diva-jsclient alvin-jsclient"
 
 for DOCKER in $DOCKERS
 do
