67a22224e0444eb72353a54e2cdfae3582a69b2a
pere <pere@8b749f981fcb>
2022-12-05 16:10:19 +0000

    add postgresql docker

--- shell/systemone-deploy-docker-instances-for-external-use.sh
+++ shell/systemone-deploy-docker-instances-for-external-use.sh
@@ -8,35 +8,77 @@ echo "Remove volumes"
 docker volume rm $(docker volume ls -q) && echo nothingToSeeMoveOnToNextCommand
 
 echo ""
-echo "Starting systemone"
-docker run --net=cora --restart always -v /mnt/data/basicstorage -p 8210:8009 --name systemone --link gatekeeper:gatekeeper --link solr:solr -d  systemone-docker:1.0-SNAPSHOT
+echo "Starting postgresql as database"
+docker run -d --name systemone-docker-postgresql \
+ -net=cora \
+ --restart unless-stopped  \
+ --net-alias=systemone-docker-postgresql \
+ -e POSTGRES_DB=systemone \
+ -e POSTGRES_USER=systemone \
+ -e POSTGRES_PASSWORD=systemone \
+ systemone-docker-postgresql:1.0-SNAPSHOT
 
 echo ""
 echo "Starting fedora for archive"
-docker run --net=cora --restart always --name systemone-docker-fedora \
+docker run -d --name systemone-docker-fedora \
+ --net=cora \
+ --restart unless-stopped \
  --mount source=systemOneArchive,target=/usr/local/tomcat/fcrepo-home/data/ocfl-root \
  --network-alias=systemone-docker-fedora \
- -d cora-docker-fedora:1.0-SNAPSHOT
+ cora-docker-fedora:1.0-SNAPSHOT
 
+echo ""
+echo "Starting systemone"
+docker run -d --net=cora --name systemone \
+ --restart unless-stopped \
+ -v /mnt/data/basicstorage \
+ -p 8210:8009 \
+ --link gatekeeper:gatekeeper \
+ --link solr:solr \
+ systemone-docker:1.0-SNAPSHOT
 
 echo ""
 echo "Starting gatekeeper"
-docker run --net=cora --restart always  --volumes-from systemone --name gatekeeper -d  systemone-docker-gatekeeper:1.0-SNAPSHOT
+docker run -d --net=cora --name gatekeeper \
+ --restart unless-stopped  \
+ --volumes-from systemone \
+ systemone-docker-gatekeeper:1.0-SNAPSHOT
+ 
 echo ""
 echo "starting idplogin"
-docker run --net=cora --restart always -e "JAVA_OPTS=-Dmain.system.domain=https://cora.epc.ub.uu.se -Dtoken.logout.url=https://cora.epc.ub.uu.se/systemone/apptokenverifier/rest/apptoken/" -p 8212:8009 --name idplogin --link gatekeeper:gatekeeper -d  cora-docker-idplogin:1.0-SNAPSHOT
+docker run -d --net=cora --name idplogin \
+ --restart unless-stopped \
+ -e "JAVA_OPTS=-Dmain.system.domain=https://cora.epc.ub.uu.se -Dtoken.logout.url=https://cora.epc.ub.uu.se/systemone/apptokenverifier/rest/apptoken/" \
+ -p 8212:8009 \
+ --link gatekeeper:gatekeeper \
+ cora-docker-idplogin:1.0-SNAPSHOT
+
 echo ""
 echo "Starting apptokenverifier"
-docker run --net=cora --restart always -e "JAVA_OPTS=-Dapptokenverifier.public.path.to.system=/systemone/apptokenverifier/rest/" --volumes-from systemone -p 8211:8009 --name apptokenverifier --link gatekeeper:gatekeeper -d  cora-docker-apptokenverifier:1.0-SNAPSHOT
+docker run -d --net=cora --name apptokenverifier \
+ --restart unless-stopped \
+ -e "JAVA_OPTS=-Dapptokenverifier.public.path.to.system=/systemone/apptokenverifier/rest/" \
+ --volumes-from systemone \
+ -p 8211:8009 \
+ --link gatekeeper:gatekeeper \
+ cora-docker-apptokenverifier:1.0-SNAPSHOT
+
 echo ""
 echo "Starting solr"
-docker run --net=cora --restart always -p 8983:8983 --name solr -d cora-solr:1.0-SNAPSHOT solr-precreate coracore /opt/solr/server/solr/configsets/coradefaultcore
+docker run -d --net=cora --name solr \
+ --restart unless-stopped \
+ -p 8983:8983  \
+ cora-solr:1.0-SNAPSHOT solr-precreate coracore /opt/solr/server/solr/configsets/coradefaultcore
 
 echo ""
 echo "starting fitnesse"
-docker run --net=cora -p 8290:8090 --name systemone-fitnesse \
+docker run -d --name systemone-fitnesse \
+ --net=cora \
+ -p 8290:8090 \
  --mount source=systemOneArchive,target=/tmp/sharedArchiveReadable/systemOne,readonly \
- --link systemone:systemone --link apptokenverifier:apptokenverifier --link idplogin:idplogin \
- -d systemone-docker-fitnesse:1.0-SNAPSHOT
+ --link systemone:systemone \
+ --link apptokenverifier:apptokenverifier \
+ --link idplogin:idplogin \
+ systemone-docker-fitnesse:1.0-SNAPSHOT
  
  
--- shell/systemone-deploy-docker-instances-for-internal-use.sh
+++ shell/systemone-deploy-docker-instances-for-internal-use.sh
@@ -1,42 +1,64 @@
 echo "Stoping dockers"
-docker stop systemone-fitnesse-test systemone-docker-fedora-test systemone-docker-fedora-test systemone-test solr-test apptokenverifier-test idplogin-test gatekeeper-test && echo nothingToSeeMoveOnToNextCommand
+docker stop systemone-fitnesse-test systemone-docker-fedora-test systemone-docker-postgresql-test systemone-docker-fedora-test systemone-test solr-test apptokenverifier-test idplogin-test gatekeeper-test && echo nothingToSeeMoveOnToNextCommand
 echo ""
 echo "Remove dockers"
-docker rm systemone-fitnesse-test systemone-docker-fedora-test systemone-docker-fedora-test systemone-test solr-test apptokenverifier-test idplogin-test gatekeeper-test && echo nothingToSeeMoveOnToNextCommand
+docker rm systemone-fitnesse-test systemone-docker-fedora-test systemone-docker-postgresql-test systemone-docker-fedora-test systemone-test solr-test apptokenverifier-test idplogin-test gatekeeper-test && echo nothingToSeeMoveOnToNextCommand
 echo ""
 echo "Remove volumes"
 docker volume rm $(docker volume ls -q) && echo nothingToSeeMoveOnToNextCommand
 
 echo ""
-echo "Starting systemone"
-docker run --net=cora-test -v /mnt/data/basicstorage --name systemone-test --link gatekeeper-test:gatekeeper --link solr-test:solr -d  systemone-docker:1.0-SNAPSHOT
+echo "Starting postgresql as database"
+docker run -d -net=cora-test --name systemone-docker-postgresql-test \
+ --net-alias=systemone-docker-postgresql \
+ -e POSTGRES_DB=systemone \
+ -e POSTGRES_USER=systemone \
+ -e POSTGRES_PASSWORD=systemone \
+ systemone-docker-postgresql:1.0-SNAPSHOT
 
 echo ""
-echo "Starting fedora for archive"
-docker run --net=cora-test --name systemone-docker-fedora-test \
+echo "Starting fedora as archive"
+docker run -d --net=cora-test --name systemone-docker-fedora-test \
  --mount source=systemOneArchiveTest,target=/usr/local/tomcat/fcrepo-home/data/ocfl-root \
  --network-alias=systemone-docker-fedora \
- -d cora-docker-fedora:1.0-SNAPSHOT
+ cora-docker-fedora:1.0-SNAPSHOT
+ 
+echo ""
+echo "Starting systemone"
+docker run -d --net=cora-test -v /mnt/data/basicstorage --name systemone-test \
+ --link gatekeeper-test:gatekeeper --link solr-test:solr 
+ systemone-docker:1.0-SNAPSHOT
+
 
 echo ""
 echo "Starting gatekeeper"
-docker run --net=cora-test --volumes-from systemone-test --name gatekeeper-test -d  systemone-docker-gatekeeper:1.0-SNAPSHOT
+docker run -d --net=cora-test --name gatekeeper-test \
+ --volumes-from systemone-test \
+ systemone-docker-gatekeeper:1.0-SNAPSHOT
+
 echo ""
 echo "starting idplogin"
-docker run --net=cora-test --name idplogin-test --link gatekeeper-test:gatekeeper -d  cora-docker-idplogin:1.0-SNAPSHOT
+docker run -d --net=cora-test --name idplogin-test \
+--link gatekeeper-test:gatekeeper\
+cora-docker-idplogin:1.0-SNAPSHOT
+
 echo ""
 echo "Starting apptokenverifier"
-docker run --net=cora-test --volumes-from systemone-test --name apptokenverifier-test --link gatekeeper-test:gatekeeper -d cora-docker-apptokenverifier:1.0-SNAPSHOT
+docker run -d --net=cora-test --name apptokenverifier-test \
+ --volumes-from systemone-test  --link gatekeeper-test:gatekeeper \
+ cora-docker-apptokenverifier:1.0-SNAPSHOT
+
 echo ""
 echo "Starting solr"
-docker run --net=cora-test --name solr-test -d cora-solr:1.0-SNAPSHOT solr-precreate coracore /opt/solr/server/solr/configsets/coradefaultcore
+docker run -d --net=cora-test --name solr-test \
+ cora-solr:1.0-SNAPSHOT solr-precreate coracore /opt/solr/server/solr/configsets/coradefaultcore
 
 echo ""
 echo "starting fitnesse"
-docker run --net=cora-test -p 8190:8090 --name systemone-fitnesse-test \
+docker run -d --net=cora-test -p 8190:8090 --name systemone-fitnesse-test \
  --mount source=systemOneArchiveTest,target=/tmp/sharedArchiveReadable/systemOne,readonly \
  --link systemone-test:systemone --link apptokenverifier-test:apptokenverifier --link idplogin-test:idplogin \
- -d systemone-docker-fitnesse:1.0-SNAPSHOT
+ systemone-docker-fitnesse:1.0-SNAPSHOT
 
 echo ""
 sleep 20
