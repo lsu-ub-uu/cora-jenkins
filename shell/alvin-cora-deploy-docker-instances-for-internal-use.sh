362e2b27f6fc8c41e450f5879af58d3db3946431
perebartrolisimo <pere.bartroli@ub.uu.se>
2022-12-07 11:02:26 +0000

    replace links with net-alias

--- shell/alvin-cora-deploy-docker-instances-for-internal-use.sh
+++ shell/alvin-cora-deploy-docker-instances-for-internal-use.sh
@@ -21,34 +21,54 @@ docker run -d --name alvin-postgresql-test \
 
 echo ""
 echo "Starting fedora for archive"
-docker run --net=alvin-cora-test  --name alvin-docker-fedora-test --network-alias=alvin-docker-fedora -d cora-docker-fedora:1.0-SNAPSHOT
+docker run -d --name alvin-docker-fedora-test \
+ --net=alvin-cora-test \
+ --network-alias=alvin-docker-fedora \
+ cora-docker-fedora:1.0-SNAPSHOT
 
 echo ""
 echo "Starting alvin"
-docker run --net=alvin-cora-test -v /mnt/data/basicstorage --name alvin-test --link alvin-gatekeeper-test:gatekeeper --link alvin-solr-test:solr --link alvin-cora-fedora-test:alvin-cora-fedora -d  alvin-docker-cora:1.0-SNAPSHOT
+docker run -d --name alvin-test \
+ --net=alvin-cora-test \ 
+ --net-alias=alvin \
+ -v /mnt/data/basicstorage
+ alvin-docker-cora:1.0-SNAPSHOT
 
 echo ""
 echo "Starting gatekeeper"
-docker run --net=alvin-cora-test --name alvin-gatekeeper-test --link alvin-cora-docker-postgresql-test:alvin-cora-docker-postgresql \
--d  alvin-docker-gatekeeper:1.0-SNAPSHOT
+docker run -d --name alvin-gatekeeper-test \
+ --net=alvin-cora-test \
+ --net-alias=gatekeeper \
+ alvin-docker-gatekeeper:1.0-SNAPSHOT
 echo ""
 echo "starting idplogin"
-docker run --net=alvin-cora-test --name alvin-idplogin-test --link alvin-gatekeeper-test:gatekeeper -e "JAVA_OPTS=-Dtoken.logout.url=https://apptokenverifier/rest/" -d  cora-docker-idplogin:1.0-SNAPSHOT
+docker run -d --net=alvin-cora-test --name alvin-idplogin-test \
+ --net-alias=idplogin \
+ -e "JAVA_OPTS=-Dtoken.logout.url=https://apptokenverifier/rest/" \
+ cora-docker-idplogin:1.0-SNAPSHOT
 
 echo ""
 echo "Starting apptokenverifier"
-docker run --net=alvin-cora-test  --name alvin-apptokenverifier-test \
- --link alvin-gatekeeper-test:gatekeeper \
+docker run -d --name alvin-apptokenverifier-test \
+ --net=alvin-cora-test \ 
+ --net-alias=apptokenverifier \
  -e "JAVA_OPTS= -Ddburl=jdbc:postgresql://alvin-postgresql:5432/alvin -Ddbusername=alvin -Ddbpassword=alvin" \
- -d  cora-docker-apptokenverifier:1.0-SNAPSHOT
+ cora-docker-apptokenverifier:1.0-SNAPSHOT
 
 echo ""
 echo "Starting solr"
-docker run --net=alvin-cora-test --name alvin-solr-test -d cora-solr:1.0-SNAPSHOT solr-precreate coracore /opt/solr/server/solr/configsets/coradefaultcore
+docker run -d --name alvin-solr-test \
+ --net=alvin-cora-test \
+  --net-alias=solr \
+ cora-solr:1.0-SNAPSHOT solr-precreate coracore /opt/solr/server/solr/configsets/coradefaultcore
 
 echo ""
 echo "Starting fitnesse"
-docker run --net=alvin-cora-test -p 8390:8090 --name alvin-fitnesse-test --link alvin-therest-test:alvin --link alvin-apptokenverifier-test:apptokenverifier --link alvin-idplogin-test:idplogin -e tokenLogoutURL=https://apptokenverifier/rest/ -d alvin-cora-docker-fitnesse:1.1-SNAPSHOT
+docker run -d --name alvin-fitnesse-test \
+ --net=alvin-cora-test \
+ -p 8390:8090  \
+ -e tokenLogoutURL=https://apptokenverifier/rest/ \
+ alvin-cora-docker-fitnesse:1.1-SNAPSHOT
 
 echo "wait for everything to start"
 sleep 20
