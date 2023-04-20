84790b509ae0be8d23a6e29bae7adf1fc424e011
egiiiiil <sixteenth.bird@gmail.com>
2023-04-20 15:18:18 +0200

    Added Spa client script

---
+++ shell/diva-spa-deploy-docker-instances-for-external-use.sh
@@ -0,0 +1,28 @@
+echo "Kill dockers"
+docker kill diva-react-spa-dev diva-react-spa-bff-dev && echo nothingToSeeMoveOnToNextCommand
+echo ""
+echo "Remove dockers"
+docker rm diva-react-spa-dev diva-react-spa-bff-dev && echo nothingToSeeMoveOnToNextCommand
+echo ""
+echo "Remove volumes"
+docker volume rm $(docker volume ls -q) && echo nothingToSeeMoveOnToNextCommand
+
+
+echo ""
+echo "Starting diva-react-spa-dev"
+docker run -d --name diva-react-spa-dev \
+    --net=diva-cora \
+    --restart=unless-stopped \
+    -p 9876:80 \
+    diva-react-spa:latest
+
+echo ""
+echo "Starting diva-react-spa-bff-dev"
+docker run -d --name diva-react-spa-bff-dev \
+    --net-alias=bff \
+    --net=diva-cora \
+    --restart=unless-stopped\
+    diva-react-spa-bff:latest
+
+echo ""
+echo "All dockers started"
