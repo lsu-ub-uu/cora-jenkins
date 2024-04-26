d1ef52fa490ed13b9ef997e5761fcb0ace62aa89
egiiiiil <sixteenth.bird@gmail.com>
2024-04-26 15:17:58 +0200

    CORA-2256 Updated diva-spa script

--- shell/diva-spa-deploy-docker-instances-for-external-use.sh
+++ shell/diva-spa-deploy-docker-instances-for-external-use.sh
@@ -1,32 +1,32 @@
 echo "Kill dockers"
-docker kill diva-react-spa-dev diva-react-spa-bff-dev && echo nothingToSeeMoveOnToNextCommand
+docker kill diva-client-gui diva-client-bff && echo nothingToSeeMoveOnToNextCommand
 echo ""
 echo "Remove dockers"
-docker rm diva-react-spa-dev diva-react-spa-bff-dev && echo nothingToSeeMoveOnToNextCommand
+docker rm diva-client-gui diva-client-bff && echo nothingToSeeMoveOnToNextCommand
 echo ""
 echo "Remove volumes"
 docker volume rm $(docker volume ls -q) && echo nothingToSeeMoveOnToNextCommand
 
 
 echo ""
-echo "Starting diva-react-spa-dev"
-docker run -d --name diva-react-spa-dev \
-    --net=diva-cora \
+echo "Starting diva-client-gui"
+docker run -d --name diva-client-gui \
     --restart=unless-stopped \
+    --net=diva-cora \
      -e VITE_BFF_API_URL=http://bff:8080/api \
      -p 9876:80 \
-    diva-react-spa:latest
+    diva-client-gui-docker:1.0-SNAPSHOT
 
 echo ""
-echo "Starting diva-react-spa-bff-dev"
-docker run -d --name diva-react-spa-bff-dev \
+echo "Starting diva-client-bff"
+docker run -d --name diva-client-bff \
     --net-alias=bff \
     --net=diva-cora \
     --restart=unless-stopped \
      -p 9877:8080 \
-     -e CORA_API_URL=https://cora.epc.ub.uu.se/diva/rest/  \
-     -e CORA_APPTOKENVERIFIER_URL=https://cora.epc.ub.uu.se/diva  \
-    diva-react-spa-bff:latest
+     -e CORA_API_URL=https://cora.epc.ub.uu.se/20240226/diva/rest  \
+     -e CORA_APPTOKENVERIFIER_URL=https://cora.epc.ub.uu.se/20240226/diva  \
+    diva-client-bff-docker:1.0-SNAPSHOT
 
 echo ""
 echo "All dockers started"
