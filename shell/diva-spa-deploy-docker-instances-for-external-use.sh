e4105d088b65a08d99d918644de43c7930df918d
egiiiiil <sixteenth.bird@gmail.com>
2024-05-02 14:01:40 +0200

    Update login url remove diva

--- shell/diva-spa-deploy-docker-instances-for-external-use.sh
+++ shell/diva-spa-deploy-docker-instances-for-external-use.sh
@@ -21,7 +21,7 @@ docker run -d --name diva-client-bff \
     --restart=unless-stopped \
     -p 9877:8080 \
     -e CORA_API_URL=http://diva:8080/diva/rest  \
-    -e CORA_LOGIN_URL=http://login:8080/diva/login/rest  \
+    -e CORA_LOGIN_URL=http://login:8080/login/rest  \
     diva-client-bff-docker:1.0-SNAPSHOT
 
 echo ""
