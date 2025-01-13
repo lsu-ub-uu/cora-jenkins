677cdfe52b7d1943936ce8ca6089e21909d99860
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-01-13 11:45:56 +0000

    fix wrong docker names

--- shell/dockerTomcatVersion.sh
+++ shell/dockerTomcatVersion.sh
@@ -1,10 +1,10 @@
 set -e
-DOCKERS="systemone idplogin login gatekeeper alvin alvin-idplogin alvin-login alvin-gatekeeper diva diva-idplogin diva-login diva-gatekeeper diva-synchronizer jsclient diva-jsclient alvin-jsclient"
+DOCKERS=$(docker ps --format "{{.Names}}")
 
 for DOCKER in $DOCKERS
 do
 	echo ""
 	echo ""
 	echo $DOCKER
-	docker exec $DOCKER /usr/local/tomcat/bin/version.sh
+	docker exec $DOCKER /usr/local/tomcat/bin/version.sh || echo "Failed to execute version.sh in $DOCKER"
 done
