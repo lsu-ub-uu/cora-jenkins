53604ff67fd02e7c3e971c56573c8dff294444f5
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-01-13 11:41:19 +0000

    fix wrong docker names

--- shell/dockerTomcatVersion.sh
+++ shell/dockerTomcatVersion.sh
@@ -1,6 +1,5 @@
 set -e
-#DOCKERS="systemone-fitnesse systemone solr idplogin login gatekeeper alvin-fitnesse alvin-cora alvin-cora-fedora alvin-solr alvin-idplogin alvin-login alvin-gatekeeper diva-fitnesse diva-cora diva-cora-fedora diva-solr diva-idplogin diva-login diva-gatekeeper diva-synchronizer jsclient diva-jsclient diva-jsclient"
-DOCKERS="systemone idplogin login gatekeeper alvin-cora alvin-idplogin alvin-login alvin-gatekeeper diva-cora diva-idplogin diva-login diva-gatekeeper diva-synchronizer jsclient diva-jsclient alvin-jsclient"
+DOCKERS="systemone idplogin login gatekeeper alvin alvin-idplogin alvin-login alvin-gatekeeper diva diva-idplogin diva-login diva-gatekeeper diva-synchronizer jsclient diva-jsclient alvin-jsclient"
 
 for DOCKER in $DOCKERS
 do
