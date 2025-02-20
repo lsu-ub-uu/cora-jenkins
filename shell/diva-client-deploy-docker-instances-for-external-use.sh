5de56702c3c70e5f3606a6cf0822ccdd5215f63d
Leo Danielsson <leo.danielsson@ub.uu.se>
2025-02-20 13:34:47 +0100

    Bump diva-client to 1.15-SNAPSHOT

--- shell/diva-client-deploy-docker-instances-for-external-use.sh
+++ shell/diva-client-deploy-docker-instances-for-external-use.sh
@@ -17,7 +17,7 @@ docker run -d --name diva-client \
     -e BASE_PATH=/divaclient \
     -e ENVIRONMENT=preview \
     -p 9876:5173 \
-    diva-client:1.14-SNAPSHOT
+    diva-client:1.15-SNAPSHOT
 
 echo ""
 echo "All dockers started"
