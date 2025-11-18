4e041e35008ea99edfec69f33c37f4d0362a2fab
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-11-18 14:14:33 +0000

    cleaning the deployment script

--- helm/deploy-system-to-anyk8s.sh
+++ helm/deploy-system-to-anyk8s.sh
@@ -1,54 +1,25 @@
 #!/bin/bash
-NAME=$1
-CLUSTER_NAME=$2
-NAMESPACE="$NAME"
-
-#Cleaning cluster before each re-deployment
-#echo ""
-#echo "Uninstalling helm kubeconfig release '$NAMESPACE' from namespace '$NAMESPACE'..."
-#helm --kubeconfig kubeconfig uninstall $NAMESPACE -n $NAMESPACE
-
-#echo ""
-#echo "Deleting secret..."
-#kubectl --kubeconfig kubeconfig delete secret ${NAME}-secret --namespace=$NAMESPACE
-#
-#echo ""
-#echo "Deleting Kubernetes namespace '$NAMESPACE'..."
-#kubectl --kubeconfig kubeconfig delete namespace $NAMESPACE
-#
-#echo ""
-#echo "Deleting persistent volumes for '$NAMESPACE'..."
-#kubectl --kubeconfig kubeconfig delete pv ${NAMESPACE}-postgres-volume
-#kubectl --kubeconfig kubeconfig delete pv ${NAMESPACE}-archive-volume
-#kubectl --kubeconfig kubeconfig delete pv ${NAMESPACE}-archive-read-only-volume
-#kubectl --kubeconfig kubeconfig delete pv ${NAMESPACE}-converted-files-volume
-#kubectl --kubeconfig kubeconfig delete pv ${NAMESPACE}-converted-files-read-only-volume
-#kubectl --kubeconfig kubeconfig delete pv ${NAME}-previewepc-credentials-read-only-volume
-#
-#echo ""
-#echo "Removing local persistent data from /mnt/minikube/$NAME/$CLUSTER_NAME..."
-#minikube ssh -- "sudo rm -rf /mnt/minikube/$NAME/$CLUSTER_NAME"
+CLUSTER_NAME=$1
+NAMESPACE=$2
+APPPLICATION_NAME=$3
+#NAMESPACE="$APPPLICATION_NAME"
 
 #Get kubeconfig. It expects the cluster is already created.
 cd helm
 curl http://test.ub.uu.se:8000/v1/config/$CLUSTER_NAME >kubeconfig
 
-#echo ""
-#echo "Creating namespace '$NAMESPACE'..."
-#kubectl --kubeconfig kubeconfig create namespace $NAMESPACE 
-
 echo ""
 echo "Applying secret"
-kubectl --kubeconfig kubeconfig apply -f ${NAME}-secret.yaml --namespace=$NAMESPACE
+kubectl --kubeconfig kubeconfig apply -f ${APPPLICATION_NAME}-secret.yaml --namespace=$NAMESPACE
 
 #echo ""
 #echo "Applying persistent volume definitions"
 #kubectl --kubeconfig kubeconfig apply -f ${NAMESPACE}-minikube-persistent-volumes.yaml --namespace=$NAMESPACE
 
 echo ""
-echo "Installing helm chart '$NAME' as release '$NAMESPACE' in namespace '$NAMESPACE'..."
+echo "Installing helm chart '$APPPLICATION_NAME' as release '$NAMESPACE' in namespace '$NAMESPACE'..."
 helm --kubeconfig kubeconfig repo update
-helm --kubeconfig kubeconfig install $NAMESPACE epc/$NAME --namespace $NAMESPACE -f ${NAME}-${CLUSTER_NAME}-values.yaml
+helm --kubeconfig kubeconfig install $NAMESPACE epc/$APPPLICATION_NAME --namespace $NAMESPACE -f ${APPPLICATION_NAME}-${CLUSTER_NAME}-values.yaml
 
 echo ""
 echo "Waiting for all pods in '$NAMESPACE' namespace to become ready (timeout: 300s)..."
