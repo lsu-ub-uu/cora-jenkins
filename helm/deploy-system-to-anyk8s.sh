3f110a1ee27421692c56a057bfef601e8eb62696
perebartrolisimo <pere.bartroli@ub.uu.se>
2025-12-18 17:40:43 +0000

    diva pre script changes

--- helm/deploy-system-to-anyk8s.sh
+++ helm/deploy-system-to-anyk8s.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
-CLUSTER_NAME=$1
-NAMESPACE=$2
-APPPLICATION_NAME=$3
-#NAMESPACE="$APPPLICATION_NAME"
 
-cd helm
-curl http://test.ub.uu.se:8000/v1/config/$CLUSTER_NAME >kubeconfig
+start(){
+	setEnvironmentVariables
+	readKubeconfig
+	applySecrets
+	installApplication
+	waitUntilAllPodsAreRunning
+}
 
-echo ""
-echo "Applying secret"
-kubectl --kubeconfig kubeconfig apply -f ${APPPLICATION_NAME}-secret.yaml --namespace=$NAMESPACE
+setEnvironmentVariables(){
+	CLUSTER_NAME=$1
+	ENVIRONMENT=$2
+	APPPLICATION_NAME=$3
+	NAMESPACE=${APPPLICATION_NAME}-${ENVIRONMENT}
+}
 
-#echo ""
-#echo "Applying persistent volume definitions"
-#kubectl --kubeconfig kubeconfig apply -f ${NAMESPACE}-minikube-persistent-volumes.yaml --namespace=$NAMESPACE
+readKubeconfig(){
+	curl http://test.ub.uu.se:8000/v1/config/$CLUSTER_NAME >kubeconfig
+}
 
-echo ""
-echo "Installing helm chart '$APPPLICATION_NAME' as release '$NAMESPACE' in namespace '$NAMESPACE'..."
-helm --kubeconfig kubeconfig repo update
-helm --kubeconfig kubeconfig install $NAMESPACE epc/$APPPLICATION_NAME --namespace $NAMESPACE -f ${NAMESPACE}-${CLUSTER_NAME}-values.yaml
+applySecrets(){
+	echo ""
+	echo "Applying secret"
+	kubectl --kubeconfig kubeconfig apply -f helm/${APPPLICATION_NAME}-secret.yaml --namespace=$NAMESPACE
+}
 
-echo ""
-echo "Waiting for all pods in '$NAMESPACE' namespace to become ready (timeout: 300s)..."
-if ! kubectl --kubeconfig kubeconfig wait --for=condition=Ready pod --all --namespace=$NAMESPACE --timeout=300s; then
-    echo "Timeout waiting for pods to become ready"
-    exit 1
-fi
+installApplication(){
+	echo ""
+	echo "Installing helm chart '$APPPLICATION_NAME' as release '$NAMESPACE' in namespace '$NAMESPACE'..."
+	helm --kubeconfig kubeconfig repo update
+	helm --kubeconfig kubeconfig install $NAMESPACE epc/$APPPLICATION_NAME --namespace $NAMESPACE -f helm/${APPPLICATION_NAME}-${ENVIRONMENT}-values.yaml
+}
 
-echo "Deployment of $NAMESPACE completed successfully on cluster $CLUSTER_NAME."
+waitUntilAllPodsAreRunning({})
+	echo ""
+	echo "Waiting for all pods in '$NAMESPACE' namespace to become ready (timeout: 300s)..."
+	if ! kubectl --kubeconfig kubeconfig wait --for=condition=Ready pod --all --namespace=$NAMESPACE --timeout=300s; then
+	    echo "Timeout waiting for pods to become ready"
+	    exit 1
+	fi
+	
+	echo "Deployment of $NAMESPACE completed successfully on cluster $CLUSTER_NAME."
+}
+
+start
--- helm/upgrade-system-to-anyk8s.sh
+++ helm/upgrade-system-to-anyk8s.sh
@@ -1,31 +1,61 @@
 #!/bin/bash
-CLUSTER_NAME=$1
-NAMESPACE=$2
-APPPLICATION_NAME=$3
+start(){
+	setEnvironmentVariables
+	readKubeconfig
+	unistallPreviousVersion
+	applySecretsAndPersistentClaims
+	installApplication
+	waitUntilAllPodsAreRunning
+}
 
-cd helm
-curl http://test.ub.uu.se:8000/v1/config/$CLUSTER_NAME > kubeconfig
+setEnvironmentVariables(){
+	CLUSTER_NAME=$1
+	ENVIRONMENT=$2
+	APPPLICATION_NAME=$3
+	NAMESPACE=${APPPLICATION_NAME}-${ENVIRONMENT}
+}
 
-echo ""
-echo "Updating helm chart '$APPPLICATION_NAME' as release '$NAMESPACE' in namespace '$NAMESPACE'..."
-helm --kubeconfig kubeconfig repo update
-helm --kubeconfig kubeconfig uninstall $NAMESPACE --namespace $NAMESPACE
+readKubeconfig(){
+	curl http://test.ub.uu.se:8000/v1/config/$CLUSTER_NAME >kubeconfig
+}
 
-kubectl  --kubeconfig kubeconfig  delete pvc $APPPLICATION_NAME-postgres-volume-claim -n $NAMESPACE
-kubectl  --kubeconfig kubeconfig  delete pvc $APPPLICATION_NAME-archive-read-write-volume-claim -n $NAMESPACE
-kubectl  --kubeconfig kubeconfig  delete pvc $APPPLICATION_NAME-credentials-read-write-volume-claim -n $NAMESPACE
-kubectl  --kubeconfig kubeconfig  delete pvc $APPPLICATION_NAME-postgres-volume-claim -n $NAMESPACE
-kubectl  --kubeconfig kubeconfig  delete pvc $APPPLICATION_NAME-converted-files-read-write-volume-claim -n $NAMESPACE
+unistallPreviousVersion(){
+	echo ""
+	echo "Updating helm chart '$APPPLICATION_NAME' as release '$NAMESPACE' in namespace '$NAMESPACE'..."
+	helm --kubeconfig kubeconfig repo update
+	helm --kubeconfig kubeconfig uninstall $NAMESPACE --namespace $NAMESPACE
+	
+	kubectl --kubeconfig kubeconfig delete pvc $APPPLICATION_NAME-postgres-volume-claim -n $NAMESPACE
+	kubectl --kubeconfig kubeconfig delete pvc $APPPLICATION_NAME-archive-read-write-volume-claim -n $NAMESPACE
+	kubectl --kubeconfig kubeconfig delete pvc $APPPLICATION_NAME-credentials-read-write-volume-claim -n $NAMESPACE
+	kubectl --kubeconfig kubeconfig delete pvc $APPPLICATION_NAME-converted-files-read-write-volume-claim -n $NAMESPACE
+}
 
-kubectl --kubeconfig kubeconfig apply -f ${NAMESPACE}-$CLUSTER_NAME-persistent-volume-claims.yaml -n $NAMESPACE
+applySecretsAndPersistentClaims(){
+	echo ""
+	echo "Applying secret"
+	kubectl --kubeconfig kubeconfig apply -f helm/${APPPLICATION_NAME}-secret.yaml --namespace=$NAMESPACE
+	
+	echo "Applying PVCs"
+	kubectl --kubeconfig kubeconfig apply -f helm/${APPPLICATION_NAME}-${ENVIRONMENT}-persistent-volume-claims.yaml -n $NAMESPACE
+}
 
-helm --kubeconfig kubeconfig install $NAMESPACE epc/$APPPLICATION_NAME --namespace $NAMESPACE -f ${NAMESPACE}-${CLUSTER_NAME}-values.yaml
+installApplication(){
+	echo ""
+	echo "Installing helm chart '$APPPLICATION_NAME' as release '$NAMESPACE' in namespace '$NAMESPACE'..."
+	helm --kubeconfig kubeconfig repo update
+	helm --kubeconfig kubeconfig install $NAMESPACE epc/$APPPLICATION_NAME --namespace $NAMESPACE -f helm/${APPPLICATION_NAME}-${ENVIRONMENT}-values.yaml
+}
 
-echo ""
-echo "Waiting for all pods in '$NAMESPACE' namespace to become ready (timeout: 300s)..."
-if ! kubectl --kubeconfig kubeconfig wait --for=condition=Ready pod --all --namespace=$NAMESPACE --timeout=300s; then
-    echo "Timeout waiting for pods to become ready"
-    exit 1
-fi
+waitUntilAllPodsAreRunning({})
+	echo ""
+	echo "Waiting for all pods in '$NAMESPACE' namespace to become ready (timeout: 300s)..."
+	if ! kubectl --kubeconfig kubeconfig wait --for=condition=Ready pod --all --namespace=$NAMESPACE --timeout=300s; then
+	    echo "Timeout waiting for pods to become ready"
+	    exit 1
+	fi
+	
+	echo "Deployment of $NAMESPACE completed successfully on cluster $CLUSTER_NAME."
+}
 
-echo "Deployment of $NAMESPACE completed successfully on cluster $CLUSTER_NAME."
+start
